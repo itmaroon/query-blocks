2b77ccc41f90213d5ef1088ee65178116005ef61
ISAMU TAKEDA <itmaroon@icloud.com>
2026-06-26 09:27:45 +0900

    6-26

--- build/blocks/post-crumbs/view.asset.php
+++ build/blocks/post-crumbs/view.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 <?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3b527f3a0f1baf5c348e');
-=======
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '74006137d723d5644765');
->>>>>>> e959fe19aea266270bd4312d760a295522918a05
